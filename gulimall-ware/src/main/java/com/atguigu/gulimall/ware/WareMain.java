62a7d7327368d931112fdc2ab63809a53f33701b
yltrcc <ttxxly@gmail.com>
2022-04-28 00:50:04 +0800

    SpringCloud Alibaba 版本 升级 2021.0.1

---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/WareMain.java
@@ -0,0 +1,22 @@
+package com.atguigu.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Package: com.yltrcc.app.ware
+ * Date：2022-01-13
+ * Time：10:12
+ * Description：TODO
+ *
+ * @author yltrcc
+ * @version 1.0
+ */
+@SpringBootApplication
+public class WareMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WareMain.class, args);
+    }
+
+}
